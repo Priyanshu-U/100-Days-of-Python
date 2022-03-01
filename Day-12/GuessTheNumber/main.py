365b724654f14b1c25a505e1e177e6ef17891c34
Priyanshu Upadhyay <upadhyaypriyanshu24@gmail.com>
2022-03-02 01:11:41 +0530

    Day-12 Added Replay Functionality

--- Day-12/GuessTheNumber/main.py
+++ Day-12/GuessTheNumber/main.py
@@ -2,55 +2,68 @@ import random
 import os
 from art import logo
 
+
 def cls():
-    os.system('cls' if os.name=='nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 
 def start_game():
     global game_state
-    game_state=True
+    game_state = True
     print(logo)
     global number_in_game
-    number_in_game=generate_number()
-    difficulty_level=input("Enter the Level of difficulty (hard/easy):")
+    number_in_game = generate_number()
+    difficulty_level = input("Enter the Level of difficulty (hard/easy):")
     global lives
-    if difficulty_level=='hard':
-        lives=5
+    if difficulty_level == 'hard':
+        lives = 5
     else:
-        lives=10
+        lives = 10
+
+
+def stop_game():
+    global game_state
+    game_state = False
+
 
 def restart_game():
     cls()
     start_game()
 
-def stop_game():
-    global game_state
-    game_state=False
+
+def check_replay():
+    replay_state = input("Would you like to play again? (y/n): ")
+    if replay_state == 'y':
+        restart_game()
+    else:
+        stop_game()
+
 
 def generate_number():
-    return random.randint(0,100)
+    return random.randint(0, 100)
+
 
 start_game()
 
 while(game_state):
-    
-    guessed_number=int(input("Guess The Number: "))
-    
-    if guessed_number>number_in_game and abs(guessed_number-number_in_game)>10:
+
+    guessed_number = int(input("Guess The Number: "))
+
+    if guessed_number > number_in_game and abs(guessed_number-number_in_game) > 10:
         print("Guess Alot Lower")
-        lives-=1
-    elif guessed_number>number_in_game and abs(guessed_number-number_in_game)<=10:
+        lives -= 1
+    elif guessed_number > number_in_game and abs(guessed_number-number_in_game) <= 10:
         print("Guess A Little Lower")
-        lives-=1
-    elif guessed_number<number_in_game and abs(guessed_number-number_in_game)<=10:
+        lives -= 1
+    elif guessed_number < number_in_game and abs(guessed_number-number_in_game) <= 10:
         print("Guess A Little Higher")
-        lives-=1
-    elif guessed_number<number_in_game and abs(guessed_number-number_in_game)>10:
+        lives -= 1
+    elif guessed_number < number_in_game and abs(guessed_number-number_in_game) > 10:
         print("Guess Alot Higher")
-        lives-=1
-    elif guessed_number==number_in_game:
-        print(f"Your guess is spot on! You Win! with {lives} left!") 
-    
-    if lives==0:
+        lives -= 1
+    elif guessed_number == number_in_game:
+        print(f"Your guess is spot on! You Win! with {lives} left!")
+        check_replay()
+    if lives == 0:
         print("Lives over! You lose!")
-        stop_game()
-        
+        check_replay()
